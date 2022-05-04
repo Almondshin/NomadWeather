7a10f9a3433e57ffdea78f7ea831be6c0c1c4ac1
Almondshin <Almondshin8@gmail.com>
2022-05-04 10:15:38 +0900

    수정

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello! I made a ReactNative app! 왜 안바뀌는데? 진챠루?</Text>
       <StatusBar style="auto" />
     </View>
   );
